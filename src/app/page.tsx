0424b94fa0285da68e4733546478e8c38fa2ea56
fitz95 <fitzgeraldnsong@gmail.com>
2024-08-11 08:55:50 +0100

    cofiguring the setup for the project as well as adding some layout and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { ArrowUpToLine, CheckCircle, Leaf } from "lucide-react"
+import Link from 'next/link'
+
+const perks = [
+{
+  name: "Instant Delivery",
+  Icon: ArrowUpToLine,
+  description: "Get the receipt of your order within seconds in your email inbox."
+},
+{
+  name: "Guaranteed Vote Count",
+  Icon: CheckCircle,
+  description: "We guarantee that your vote will be counted and will be anonymous."
+},
+{
+  name: "For the Planet",
+  Icon: Leaf,
+  description: "We have pledged 1% of our profits to the environment.",
+
+}
+
+]
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+  return <>
+   <MaxWidthWrapper>
+    <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Voting Platform for
+        <span className="text-blue-600"> Miss Twitter 237</span>
+      </h1>
+      <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+        Welcome to your Miss Twitter237. We are here to help you vote for your favorite candidate.
+      </p>
+      <div className="flex flex-col sm:flex-row gap-4 mt-6">
+        <Link href="/products" className={buttonVariants()}>Browse Candidates</Link>
+        <Button  variant="ghost" className="mt-4 sm:mt-0">Leading Candidates &rarr;</Button>
+      </div>
+    </div>
+
+    {/* Candidates*/}
+  </MaxWidthWrapper>
+  <section className="border border-gray-200 bg-gray-50">
+  <MaxWidthWrapper>
+      
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Why Vote with Us?</h2>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Here are some of the perks of voting with us
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="grid grid-cols-1 gap-8 mt-6 sm:grid-cols-3">
+          {perks.map(({ Icon, name, description }) => (
+            <div key={name} className="flex flex-col items-center">
+              <div className="h-16 w-16 flex  items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                <Icon className="w-12 h-12 " />
+              </div>
+              <h3 className="mt-4 text-lg font-semibold text-gray-900">{name}</h3>
+              <p className="mt-2 text-base text-muted-foreground">{description}</p>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    </MaxWidthWrapper>
+    <MaxWidthWrapper>
+      
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Leading Candidates</h2>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Here are the top 3 candidates for this year's Miss Twitter237
+        </p>
+        {/* <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="/products" className={buttonVariants()}>Browse Candidates</Link>
+          <Button  variant="ghost" className="mt-4 sm:mt-0">View All Candidates &rarr;</Button>
+        </div> */}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </MaxWidthWrapper>
+  </section>
+  </>
 }
