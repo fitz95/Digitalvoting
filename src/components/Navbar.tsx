b184e29dfe079d5b352323d23d6b8865aa215793
fitz95 <fitzgeraldnsong@gmail.com>
2024-09-10 09:47:19 +0100

    adding different parts of the navrbar with relation to the user commands conditions for navbar with and without user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
-import { Link } from "lucide-react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import Cart from "./Cart";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
+
+    const user = null
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
         <header className="relative bg-white">
@@ -21,8 +25,45 @@ const Navbar = () => {
                             <NavItems />
                         </div>
                         <div className="ml-auto flex items-center">
+                            <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                                {user ? null : (
+                                    <Link href="/sign-in" className={buttonVariants({variant: 'ghost'})}>Sign In</Link>
+                                )}
 
+                                {user ? null :(
+                                    <span 
+                                    className='h-6 w-px bg-gray-200' 
+                                    aria-hidden= 'true'  
+                                    />
+                                )}
 
+                                {user ? <p> </p> : (
+                                    <Link 
+                                    href="/sign-up"
+                                    className={buttonVariants({variant: 'ghost'
+                                    })}>Create Account
+                                    </Link>
+                                )}
+
+                                {user ? (
+                                    <span 
+                                    className='h-6 w-px bg-gray-200' 
+                                    aria-hidden= 'true'  
+                                    />
+                                ) : null}
+                                
+                                {user ? null :(
+                                 <div className="flex lg:ml-6 lg:space-x-6">
+                                    <span 
+                                    className='h-6 w-px bg-gray-200' 
+                                    aria-hidden= 'true'  
+                                    />
+                                    </div>
+                                )}
+
+                                <div className="ml-4 flow-root lg:ml-6"></div>
+                                  <Cart /> 
+                            </div>
                         </div>
                     </div>
                 </div>
