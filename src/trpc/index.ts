98999cb08c4d84d4fea31f379383a83807284596
fitz95 <fitzgeraldnsong@gmail.com>
2024-09-16 08:54:23 +0100

    configure trpc for next app

---
+++ src/trpc/index.ts
@@ -0,0 +1,10 @@
+import { any } from "zod"
+import { publicProcedure, router } from "./trpc"
+
+export const appRouter = router({
+    anyApiRoute: publicProcedure.query(() =>{
+        return 'hello'
+    }),
+})
+
+export type AppRouter = typeof appRouter
